fbcc97938c1342182451663c69fc1e0c0c7ac1ea
codyking <csking2@loyola.edu>
2024-11-05 08:57:54 -0500

    main.py initial

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import random
-
-
-result_list = []
-
-def dice_roll():
-    roll1 = random.randint(1, 6)
-    roll2 = random.randint(1, 6)
-    roll_sum = roll1 + roll2
-    return roll_sum
-
 count = 0
-plays = input('Enter how many times you want to roll: ')
-plays = int(plays)
-while count < plays:
-    roll_sum = dice_roll()
-    result_list[roll_sum] += 1
-    print(result_list)
+sum_counts = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+num_rolls = 0
 
-count += 1
 
+def roll_of_dice():
+    global count
+    global num_rolls
+    while count < num_rolls:
+        roll1 = random.randint(1, 6)
+        roll2 = random.randint(1, 6)
+        roll_sum = roll2 + roll1
+        sum_counts[roll_sum] += 1
+        count += 1
+
+
+def main():
+    global num_rolls
+    num_rolls = int(input("How many dice do you want to roll? "))
+    while num_rolls > 0:
+        if num_rolls > 0:
+            roll_of_dice()
+            print(f'Rolling {num_rolls} pairs of dice')
+            for sum_val in range(2, 13):
+                print(f"Sum of {sum_val}: {'*' * sum_counts[sum_val]}")
+            break
+
+    num_rolls = int(input("Error, Try again. How many dice do you want to roll? "))
+
+main()
 
 
 
